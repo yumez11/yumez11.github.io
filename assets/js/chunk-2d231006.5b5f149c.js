31a235f21bd7d13086a663588f94da08eff8ef09
yumez <yumez@qq.com>
2021-12-31 09:47:38 +0800

    add blog web

---
+++ assets/js/chunk-2d231006.5b5f149c.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d231006"],{ef4c:function(n,e,c){"use strict";c.r(e);var t=c("7a23"),a={class:"about"},s=Object(t["f"])("h1",null,"This is an about page",-1),u=[s];function o(n,e){return Object(t["r"])(),Object(t["e"])("div",a,u)}var r=c("6b0d"),b=c.n(r);const i={},d=b()(i,[["render",o]]);e["default"]=d}}]);
